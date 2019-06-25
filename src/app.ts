0c25939d06d88539c081b57f58b7a0a53f185dd7
Hubert Rzadzinski <h.rzadzinski@gmail.com>
2019-06-25 07:27:54 +0200

    Updated JSON list; deleted unused path

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 
 const app: express.Application = express();
-const countriesRoutes = require('../api/routes/countries');
+const countriesRoutes = require('../dist/api/routes/countries.js');
 
 app.use('/countries', countriesRoutes);
 module.exports = app;
